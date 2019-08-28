3ab9592e3000b911d839f39e94594324a9e039e0
Justin Dudley <justindudley99@gmail.com>
2019-08-28 12:08:02 -0400

    added method CheckAmountIsPositive

--- AccountClassProject/Account.cs
+++ AccountClassProject/Account.cs
@@ -6,22 +6,49 @@ namespace AccountClassProject {
     class Account {
 
         public string AccountNumber { get; set; }
-        // we can set a beginning balance, like this, if we want: (see the stuff after the = )
-        public decimal Balance { get; set; } = 0.0M; // note the "M".  "Treat this as a decimal, not a double. 
+        // Balance has a private set, so can't set it by accident.  Also, note we have set a beginning balance
+        // public decimal Balance { get; private set; }     // Note:  public property with a private set.  You can do this if desired.
+        private decimal Balance { get; set; } = 0.0M; // note the "M".  "Treat this as a decimal, not a double.
         public string Description { get; set; }     // user-definable description
 
         // need to get used to this:    var   Get(  <void>  )   [empty parenths]
         //                              void  Set(  var  )
-        public decimal GetBalance() {       
+
+        // we don't want just anyone to get a balance.  We use a get method, and we make Balance PRIVATE. Outside the class, no one can see Account.Balance (?)
+        public decimal GetBalance() {      
             return Balance;
         }
+        // create private method, only used INSIDE account class.
+        private bool CheckAmountIsPositive(decimal amount) {
+            if (amount < 0) {
+                Console.WriteLine("Amount cannot be negative"); // msg to user is handled inside this method, like I did with HamiltonDb
+                return false;  // this is usually the best-practice way to handle errors.  Just "return".  Don't do an else-clause.
+            }
+            return true;
+            
+        }
+        // scope: When "Deposit" is called, a variable of type decimal is created.  It only exists within that method. And I assume
+        // it lasts for a very short time?  It is given a location in RAM, a value, and a name, for just a moment?  Hmm, after CheckAmountIsPositive
+        // is called, and control is returned to Account.Deposit, the variable still exists.  So it doesn't get destroyed when CheckAmount.. is called.
+        // Maybe it gets destroyed when we return;.
         public void Deposit(decimal Amount) {
-            Balance += Amount;
+            var valid = CheckAmountIsPositive(Amount);
+            if (valid == true) {
+                Balance += Amount;
+            }
         }
         public void Withdraw(decimal Amount) {
-            Balance -= Amount;
+            // need to guard against overdraws:
+            var valid = CheckAmountIsPositive(Amount);
+            if(valid == true) {
+                if(Amount > Balance) {
+                    Console.WriteLine("Insufficient funds.");
+                } else {
+                Balance -= Amount;
+                }
+            }
         }
         
-
+        // Time to refactor.  Same functionality, streamlined
     }
 }
--- AccountClassProject/Program.cs
+++ AccountClassProject/Program.cs
@@ -6,6 +6,29 @@ namespace AccountClassProject {
 
             //now, create an instance of the account
             var acct1 = new Account();
+            acct1.AccountNumber = "12345";
+            // acct1.Balance is now private :)
+            // acct1.Balance has been replaced by acct1.GetBalance
+            // the general rule:  You should not let other classes access your properties. Use get/set methods instead.
+            // acct1.Balance = 100000M;  // You don't have to set this, AND FURTHERMORE, we should make it impossible to set the balance in this way
+
+            acct1.Description = "Primary Checking";
+            acct1.Withdraw(50000);
+            Console.WriteLine($"{acct1.AccountNumber} {acct1.Description} {acct1.GetBalance()}");
+            acct1.Withdraw(52000);  // the system will turn this integer into a decimal
+            Console.WriteLine($"{acct1.AccountNumber} {acct1.Description} {acct1.GetBalance()}");
+            acct1.Deposit(5);
+            Console.WriteLine($"{acct1.AccountNumber} {acct1.Description} {acct1.GetBalance()}");
+
+            //now we make a variable.  Not lowercase
+            var balance = acct1.GetBalance();
+            Console.WriteLine($"{balance}");
+
+            // WE NEED SOME ENHANCEMENTS:
+            //   - No setting the balance directly; only through deposit/withdrawal
+            //   - Cannot overdraw
+            //   - protect against duplicate account numbers
+
 
         }
     }
